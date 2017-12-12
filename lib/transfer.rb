82c4321f43ef474bbd2605eeecd730c8bc473564
John Wojciechowski <jwojo13@gmail.com>
2017-12-12 03:10:48 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.valid?
+    if self.valid?
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
