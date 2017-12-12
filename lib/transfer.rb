bab677779a18de5b27ef683d18875f73ae402bf1
John Wojciechowski <jwojo13@gmail.com>
2017-12-12 04:01:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -32,7 +32,7 @@ class Transfer
     end
 
   def reverse_transfer
-    @sender.deposit(amount)
-    @receiver.deposit(-amount)
+    sender.deposit(amount)
+    receiver.deposit(-amount)
   end
 end
