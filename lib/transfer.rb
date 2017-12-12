be5568849be027f54ccb54ff016be42fe830d6c3
John Wojciechowski <jwojo13@gmail.com>
2017-12-12 04:03:10 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ class Transfer
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
-    end
+  end
 
   def reverse_transfer
     if @status == "complete"
