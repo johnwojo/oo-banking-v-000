ee118620c76c043558be6e5e7961c250d3d5c8ad
John Wojciechowski <jwojo13@gmail.com>
2017-12-12 03:22:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
       @status = "complete"
-      @@all = []
+      @@all << @amount
     else
       puts "Transaction rejected. Please check your account balance."
     end
