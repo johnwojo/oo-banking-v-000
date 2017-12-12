c66015450badee2009e9b1e5ca7ecef0993ec68b
John Wojciechowski <jwojo13@gmail.com>
2017-12-12 03:31:08 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       return "Transaction rejected. Please check your account balance."
     elsif
       @@all.include? @amount
-      return "cats life"
+      return "Transaction rejected. Please check your account balance."
     else
       @sender.balance = @sender.balance - @amount
       @receiver.balance = @receiver.balance + @amount
